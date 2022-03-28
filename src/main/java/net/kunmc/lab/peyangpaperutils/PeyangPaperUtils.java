c0293ded7798ce1b1ee02681b959583d6c39f848
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-03-29 03:59:40 +0900

    First commit

---
+++ src/main/java/net/kunmc/lab/peyangpaperutils/PeyangPaperUtils.java
@@ -0,0 +1,20 @@
+package net.kunmc.lab.peyangpaperutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PeyangPaperUtils extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
