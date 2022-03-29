bbefa29d39124d117b6bf60caebe9ca5cdaaf4d7
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-03-29 09:42:45 +0900

    docs: Pair javadoc

--- src/main/java/net/kunmc/lab/peyangpaperutils/lib/utils/Pair.java
+++ src/main/java/net/kunmc/lab/peyangpaperutils/lib/utils/Pair.java
@@ -3,6 +3,9 @@ package net.kunmc.lab.peyangpaperutils.lib.utils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * ペアを表すクラス
+ */
 @Data
 @AllArgsConstructor
 public class Pair<L, R>
@@ -10,6 +13,15 @@ public class Pair<L, R>
     private L left;
     private R right;
 
+    /**
+     * ペアを生成する
+     *
+     * @param left  左側の値
+     * @param right 右側の値
+     * @param <L>   左側の型
+     * @param <R>   右側の型
+     * @return ペア
+     */
     public static <L, R> Pair<L, R> of(L left, R right)
     {
         return new Pair<>(left, right);
