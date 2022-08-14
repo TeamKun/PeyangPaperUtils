107b5af5aa7eec0107ca619e663f50e0ec0e316b
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2022-08-14 10:35:24 +0900

    feat: pre-load

--- src/main/java/net/kunmc/lab/peyangpaperutils/PeyangPaperUtils.java
+++ src/main/java/net/kunmc/lab/peyangpaperutils/PeyangPaperUtils.java
@@ -6,6 +6,15 @@ import net.kunmc.lab.peyangpaperutils.lib.terminal.InputManager;
 import net.kunmc.lab.peyangpaperutils.plugin.commands.PeyangDebugCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.jar.JarFile;
+
 public final class PeyangPaperUtils extends JavaPlugin
 {
     @Getter
@@ -15,6 +24,8 @@ public final class PeyangPaperUtils extends JavaPlugin
     private InputManager inputManager;
 
     private CommandManager pluginCommandManager;
+
+    private List<String> classes;
 
     public PeyangPaperUtils()
     {
@@ -27,6 +38,71 @@ public final class PeyangPaperUtils extends JavaPlugin
         this.inputManager = new InputManager(this);
         this.pluginCommandManager = new CommandManager(this, "peyangutils", "PeyangUtilsDebug", "peyangutils");
         this.pluginCommandManager.registerCommand("debug", new PeyangDebugCommand());
+
+        this.classes = new ArrayList<>();
+
+        this.getLogger().info("Loading classes...");
+
+        this.loadClasses();
+    }
+
+    private void loadClasses()
+    {
+        try (JarFile jarFile = new JarFile(this.getFile()))
+        {
+            jarFile.stream()
+                    .filter(jarEntry -> jarEntry.getName().endsWith(".class"))
+                    .map(jarEntry -> jarEntry.getName().replaceAll("/", "."))
+                    .map(className -> className.substring(0, className.length() - ".class".length()))
+                    .forEach(this.classes::add);
+        }
+        catch (Exception e)
+        {
+            this.getLogger().severe("Failed to load classes!");
+            e.printStackTrace();
+        }
+
+        this.getLogger().info("Enumerated " + this.classes.size() + " classes.");
+
+        this.getLogger().info("Loading classes...");
+
+        String urlString = "jar:file:" + this.getFile().getAbsolutePath() + "!/";
+        URL url = null;
+        try
+        {
+            url = new URL(urlString);
+        }
+        catch (MalformedURLException e)
+        {
+            throw new IllegalStateException(e);
+        }
+
+        try (URLClassLoader ulc = new URLClassLoader(new URL[]{url}, ClassLoader.getSystemClassLoader()))
+        {
+
+            AtomicLong count = new AtomicLong(0);
+
+            this.classes.forEach(className ->
+            {
+                try
+                {
+                    ulc.loadClass(className);
+                    count.incrementAndGet();
+                }
+                catch (Exception e)
+                {
+                    this.getLogger().severe("Failed to load class " + className + "!");
+                    e.printStackTrace();
+                }
+            });
+
+            this.getLogger().info("Loaded (" + count.get() + "/" + this.classes.size() + ") classes.");
+
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
