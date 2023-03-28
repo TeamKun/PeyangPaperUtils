2cc9d88081ccf10b37f274915dceec768a600ae1
Peyang <peyang@peya.tokyo>
2023-03-29 02:23:46 +0900

    feat: Version api

---
+++ src/main/java/net/kunmc/lab/peyangpaperutils/versioning/VersionComparator.java
@@ -0,0 +1,12 @@
+package net.kunmc.lab.peyangpaperutils.versioning;
+
+import java.util.Comparator;
+
+public class VersionComparator implements Comparator<Version>
+{
+    @Override
+    public int compare(Version o1, Version o2)
+    {
+        return o1.compareTo(o2);
+    }
+}
